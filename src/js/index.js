cb96c94c440f846c3868941359a3d4979f753b90
André Vilarinho <andrevilarinho@gmail.com>
2024-02-27 18:49:42 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,22 @@
+const campo = document.querySelectorAll(".campo")
+const campoObrigatorio = document.querySelectorAll(".campo-obrigatorio")
+const botao = document.getElementById("btn-enviar")
 
-function campoPreenchido() {
-    alert("Funciona, carai!!!")
-
-    const campo = document.querySelectorAll(".campo")
-    console.log(campo)
+botao.addEventListener('click', () => {
     
     campo.forEach( input => {
-        input.addEventListener('change', () => {
-            if (input.value !== "") {
-                input.classList.add("campo-preenchido")
-            } else {
-                input.classList.remove("campo-preenchido")
-            }
-        })
+        if (input.value !== "") {
+            input.classList.toggle("campo-preenchido")
+        }
     })
-}
+
+    campo.forEach( input => {
+        if (input.value === "") {
+            input.classList.toggle("campo-nao-preenchido")
+        }
+    })
+
+    campoObrigatorio.forEach( ativo => {
+        ativo.classList.toggle("ativo")
+    })
+})
